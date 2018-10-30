d872e83e9861abe4ef4b0ba556b79bb49d7c66b2
Rannes Pärn <rannes.parn@ektaco.ee>
2018-10-30 19:20:27 +0200

    Added 2 new Exception types.

---
+++ BusinessRegister/src/BusinessRegister.Dal/Exceptions/BrArgumentException.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using BusinessRegister.Dal.Models;
+
+namespace BusinessRegister.Dal.Exceptions
+{
+    /// <inheritdoc cref="ArgumentException" />
+    public class BrArgumentException : ArgumentException, IBrBaseException
+    {
+        /// <inheritdoc />
+        public ResultCode Result { get; }
+
+        /// <summary>
+        /// Exception constructor
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="argumentName">Argument that caused the exception</param>
+        /// <param name="result"><see cref="ResultCode"/></param>
+        public BrArgumentException(string message, string argumentName, ResultCode result)
+            : base(message, argumentName)
+        {
+            Result = result;
+        }
+
+        /// <summary>
+        /// Exception constructor
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="argumentName">Argument that caused the exception</param>
+        /// <param name="result"><see cref="ResultCode"/></param>
+        /// <param name="innerException">Original exception</param>
+        public BrArgumentException(string message, string argumentName, ResultCode result, Exception innerException)
+            : base(message, argumentName, innerException)
+        {
+            Result = result;
+        }
+
+        /// <summary>
+        /// Exception constructor
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="result"><see cref="ResultCode"/></param>
+        /// <param name="innerException">Original exception</param>
+        public BrArgumentException(string message, ResultCode result, Exception innerException)
+            : base(message, innerException)
+        {
+            Result = result;
+        }
+
+        /// <summary>
+        /// Exception constructor
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="result"><see cref="ResultCode"/></param>
+        public BrArgumentException(string message, ResultCode result)
+            : base(message)
+        {
+            Result = result;
+        }
+
+        /// <summary>
+        /// Exception constructor
+        /// </summary>
+        /// <param name="result"><see cref="ResultCode"/></param>
+        public BrArgumentException(ResultCode result)
+        {
+            Result = result;
+        }
+    }
+}
---
+++ BusinessRegister/src/BusinessRegister.Dal/Exceptions/BrInvalidOperationException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BusinessRegister.Dal.Models;
+
+namespace BusinessRegister.Dal.Exceptions
+{
+    /// <inheritdoc cref="InvalidOperationException" />
+    public class BrInvalidOperationException : InvalidOperationException, IBrBaseException
+    {
+        /// <inheritdoc />
+        public ResultCode Result { get; }
+
+        /// <summary>
+        /// Exception constructor
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="result"><see cref="ResultCode"/></param>
+        public BrInvalidOperationException(string message, ResultCode result)
+            : base(message)
+        {
+            Result = result;
+        }
+
+        /// <summary>
+        /// Exception constructor
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="result"><see cref="ResultCode"/></param>
+        /// <param name="innerException">Original exception</param>
+        public BrInvalidOperationException(string message, ResultCode result, Exception innerException)
+            : base(message, innerException)
+        {
+            Result = result;
+        }
+
+        /// <summary>
+        /// Exception constructor
+        /// </summary>
+        /// <param name="result"><see cref="ResultCode"/></param>
+        public BrInvalidOperationException(ResultCode result)
+        {
+            Result = result;
+        }
+    }
+}
